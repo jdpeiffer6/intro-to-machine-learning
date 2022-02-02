210cc01431b315a68bcc91a55f2c4c07960f74cf
unknown <jd@1350-WL-05137.accounts.ad.wustl.edu>
2022-02-01 19:55:36 -0600

    Some hw1 stuff

---
+++ hw1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python2.7
+# Homework 1 Code
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def perceptron_learn(data_in):
+    # Run PLA on the input data
+    #
+    # Inputs: data_in: Assumed to be a matrix with each row representing an
+    #                (x,y) pair, with the x vector augmented with an
+    #                initial 1 (i.e., x_0), and the label (y) in the last column
+    # Outputs: w: A weight vector (should linearly separate the data if it is linearly separable)
+    #        iterations: The number of iterations the algorithm ran for
+
+    # Your code here, assign the proper values to w and iterations:
+
+    return w, iterations
+
+
+def perceptron_experiment(N, d, num_exp):
+    # Code for running the perceptron experiment in HW1
+    # Implement the dataset construction and call perceptron_learn; repeat num_exp times
+    #
+    # Inputs: N is the number of training data points
+    #         d is the dimensionality of each data point (before adding x_0)
+    #         num_exp is the number of times to repeat the experiment
+    # Outputs: num_iters is the # of iterations PLA takes for each experiment
+    #          bounds_minus_ni is the difference between the theoretical bound and the actual number of iterations
+    # (both the outputs should be num_exp long)
+
+    # Initialize the return variables
+    num_iters = np.zeros((num_exp,))
+    bounds_minus_ni = np.zeros((num_exp,))
+
+    # Your code here, assign the values to num_iters and bounds_minus_ni:
+
+    return num_iters, bounds_minus_ni
+
+
+def main():
+    print("Running the experiment...")
+    num_iters, bounds_minus_ni = perceptron_experiment(100, 10, 1000)
+
+    print("Printing histogram...")
+    plt.hist(num_iters)
+    plt.title("Histogram of Number of Iterations")
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Count")
+    plt.show()
+
+    print("Printing second histogram")
+    plt.hist(np.log(bounds_minus_ni))
+    plt.title("Bounds Minus Iterations")
+    plt.xlabel("Log Difference of Theoretical Bounds and Actual # Iterations")
+    plt.ylabel("Count")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
